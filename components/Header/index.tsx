6b0c9706ff338f8535d526d120a54cb15b396d47
mnaffeti <mohamedaziz.naffeti@esprit.tn>
2025-12-08 16:22:33 +0100

    tag: v-0

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -39,7 +39,7 @@ const Header = () => {
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/">
             <Image
-              src="/images/logo/logo_1.png"
+              src="/images/logo/logo_1.PNG"
               alt="Tunisie Societe Logo"
               width={160}
               height={160}
